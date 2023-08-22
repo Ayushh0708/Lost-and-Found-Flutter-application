825cb521be0b40568ebe82852baab12cc2e7d533
Ayushh0708 <paliwalaayush717@gmail.com>
2023-08-22 15:32:34 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: Text("This sis my first flutter"),
+      ),
+      body:Center(
+        child:  Container(
+        width: 300,
+        height: 250,
+        color:Colors.cyanAccent,
+        child: Center(child: Text('Hello Ayush, I love you!',style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 20))),
+        
+      )
+      ),
+      
+    );
+  }
+}
---
+++ lib/practice.dart
@@ -0,0 +1,13 @@
+main(){
+
+var contact={
+
+  'key1':'value 1',
+  'key2':2,
+  'key3':'asfhh1'
+
+};
+
+print(contact);
+
+}
